6f8ebcdfc0d30fd9dcb278ddad96c7f96a690be0
LEMOINE Tristan <tristan.lemoine@telecomnancy.eu>
2024-12-09 09:54:11 +0100

    added template

---
+++ utils/template.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "strings"
+    _ "embed"
+)
+
+//go:embed test.txt
+var inputDay string
+
+func parser(){
+    return
+}
+
+func Part1(){
+    return
+}
+func Part2(){
+    return
+}
+
+func main(){
+    fmt.Println("PART1:",Part1(parser()))
+    fmt.Println("PART 2:",Part2(parser()))
+}
