cae50c45367f4de65fffdf3c3fd431c3f3b39e88
Bar0lg <barolgjeu@gmail.com>
2024-12-15 23:08:24 +0100

    Improved mkday

--- mkday.sh
+++ mkday.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-mkdir Day$1
-cp utils/template.go Day$1/main.go
-cp utils/utils.go Day$1
+source .env
+mkdir -p Day$1
+cp -n utils/template.go Day$1/main.go
+cp -n utils/utils.go Day$1
 touch Day$1/test.txt
-touch Day$1/input.txt
+curl -b "session=$SESSION" https://adventofcode.com/2024/day/$1/input > Day$1/input.txt
